f5190c8ec6001dd74829bfae78d3e248177cc8f8
Antonio Quental <aquental@users.noreply.github.com>
2024-08-19 11:51:59 -0300

    Added commands at README

--- README.md
+++ README.md
@@ -2,20 +2,28 @@
 
 A sample project using [Extism](https://extism.org/) for Rust
 
-### install Extism CLI
+## install Extism CLI
 
 ```shell
 curl -s https://get.extism.org/cli | sh
 ```
 
-### add rust Wasm target
+## add rust Wasm target
 
 ```shell
 rustup target add wasm32-unknown-unknown
 ```
 
+## inside plugin directory
+
 ### build sample
 
 ```shell
 cargo build --target wasm32-unknown-unknown
+```
+
+### run the sample
+
+```shell
+extism call target/wasm32-unknown-unknown/debug/rust_pdk_template.wasm greet --input "Benjamin"
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use extism::*;
 
 fn main() {
-    let url =
-        Wasm::url("https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm");
+    // let url =
+    //     Wasm::url("https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm");
+    let url = Wasm::url("./plugin/target/wasm32-unknown-unknown/debug/rust_pdk_template.wasm");
     let manifest = Manifest::new([url]);
     let mut plugin = Plugin::new(&manifest, [], true).unwrap();
 
+    // let res = plugin
+    //     .call::<&str, &str>("count_vowels", "Hello, world!")
+    //     .unwrap();
     let res = plugin
-        .call::<&str, &str>("count_vowels", "Hello, world!")
+        .call::<&str, &str>("Hello", "Antonio Quental")
         .unwrap();
     println!("{}\n", res);
-
-    
 }
