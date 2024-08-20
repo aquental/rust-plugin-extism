fa2220479dceaf3434a5ffc96f063becebcd74ed
Antonio Quental <aquental@users.noreply.github.com>
2024-08-20 10:49:55 -0300

    updated plugin url

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use extism::*;
 fn main() {
     // let url =
     //     Wasm::url("https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm");
-    let url = Wasm::url("./plugin/target/wasm32-unknown-unknown/debug/rust_pdk_template.wasm");
+    let url = Wasm::url("file:///Users/aquental/projects/rust/rust-plugin-extism/plugin/target/wasm32-unknown-unknown/debug/rust_pdk_template.wasm");
     let manifest = Manifest::new([url]);
     let mut plugin = Plugin::new(&manifest, [], true).unwrap();
 
